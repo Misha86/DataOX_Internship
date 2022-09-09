d8cf279a456b13aeaa20667375e221f25a6f6bd1
Misha86 <mpolishchuk86@gmail.com>
2022-09-09 15:46:08 +0300

    Added models.py to the project

---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column
+from sqlalchemy import (Integer, String, Date, Text, Float)
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Apartment(Base):
+
+    __tablename__ = "apartment"
+
+    id = Column(Integer, primary_key=True)
+    image = Column(String(300))
+    title = Column(String(100))
+    date = Column(Date)
+    city = Column(String(100))
+    description = Column(Text)
+    beds = Column(Integer)
+    price = Column(Float(decimal_return_scale=2))
+    currency = Column(String(1))
+
+    def __repr__(self):
+        return self.title
